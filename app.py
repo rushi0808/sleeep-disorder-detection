9c0eead6bf6584bf4038688baef7d54e96f41a7d
rushin236 <rushikeshshindesandesh@gmail.com>
2023-12-20 18:02:52 +0530

    updated flask app and requirements.txt

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import math
 import pickle
 
 import numpy as np
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates", static_folder="staticFiles")
-model = pickle.load(open(r"./model/model.pkl", "rb"))
 
 
 @app.route("/")
@@ -15,27 +14,12 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    int_features = [[int(x) for x in request.form.values()]]
-    final_features = np.array(int_features)
-    prediction = model.predict(final_features)
-    print(final_features)
-    print(prediction)
-    if prediction == 0:
-        return render_template(
-            "index.html",
-            prediction_text="You have no sleep disorder!".format(prediction),
-        )
-    elif prediction == 1:
-        return render_template(
-            "index.html",
-            prediction_text="You have sleep Apnea Please Visit a doctor!".format(prediction),
-        )
-    else:
-        return render_template(
-            "index.html",
-            prediction_text="You have Insomnia Please Visit a doctor!".format(prediction),
-        )
+    data = request.form.items()
+    data = list(data)
+    for key, value in data:
+        print(f"{key}: {value}")
+    return render_template("index.html", prediction_text=data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)

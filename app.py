24b73f01aca0a2dcfacbeb048bd57826c1258795
rushin236 <rushikeshshindesandesh@gmail.com>
2023-12-23 00:59:36 +0530

    build core website structure

--- app.py
+++ app.py
@@ -2,23 +2,41 @@ import math
 import pickle
 
 import numpy as np
-from flask import Flask, render_template, request
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+)
 
-app = Flask(__name__, template_folder="templates", static_folder="staticFiles")
+app = Flask(__name__)
+
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
 
 @app.route("/")
+def index():
+    return redirect("/home")
+
+
+@app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.form.items()
-    data = list(data)
-    for key, value in data:
-        print(f"{key}: {value}")
-    return render_template("index.html", prediction_text=data)
+@app.route("/detect")
+def detect():
+    return render_template("detect.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
